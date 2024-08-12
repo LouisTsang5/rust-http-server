1ed255b339dc46a1d0f8b1040772500dc07a5b95
louistsang <louis.tsang@ricoh.com.hk>
2024-08-12 16:03:31 +0800

    add tokio async run time to handle multiple requests at once

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 use std::{
     collections::HashMap,
-    error::Error,
-    fs::File,
-    io::{self, stdout, BufReader, Cursor, Read, Write},
-    net::{TcpListener, TcpStream},
+    io::Cursor,
     path::Path,
+    pin::Pin,
+    task::{Context, Poll},
+};
+use tokio::{
+    fs::File,
+    io::{self, stdout, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream},
+    task,
 };
 
 const HEADER_BUFF_INIT_SIZE: usize = 1024;
 const RES_ROOT_FOLDER: &str = "res";
 
-fn read_headers_buff<R: Read>(stream: &mut R) -> Result<Vec<u8>, io::Error> {
+async fn read_headers_buff<R: AsyncRead + Unpin>(stream: &mut R) -> Result<Vec<u8>, io::Error> {
     let mut res = Vec::with_capacity(HEADER_BUFF_INIT_SIZE);
     let mut t_bytes_read = 0;
     const END_OF_HEADER: &[u8] = b"\r\n\r\n";
@@ -19,7 +24,16 @@ fn read_headers_buff<R: Read>(stream: &mut R) -> Result<Vec<u8>, io::Error> {
         || &res[t_bytes_read - END_OF_HEADER.len()..] != END_OF_HEADER
     {
         let mut buff = [0u8; READ_SIZE];
-        let bytes_read = stream.read(&mut buff)?;
+        let bytes_read = stream.read(&mut buff).await?;
+
+        // Check if end of file
+        if bytes_read <= 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::UnexpectedEof,
+                "Unexpected end of file",
+            ));
+        }
+
         t_bytes_read += bytes_read;
         res.extend_from_slice(&buff);
     }
@@ -66,32 +80,89 @@ pub struct TeeWriter<L, R> {
     right: R,
 }
 
-impl<L: Write, R: Write> TeeWriter<L, R> {
+impl<L: AsyncWrite, R: AsyncWrite> TeeWriter<L, R> {
     fn new(left: L, right: R) -> Self {
         Self { left, right }
     }
 }
 
-impl<L: Write, R: Write> Write for TeeWriter<L, R> {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let n = self.left.write(&buf[..])?;
-        self.right.write_all(&buf[..n])?;
-        Ok(n)
+impl<L: AsyncWrite + Unpin, R: AsyncWrite + Unpin> AsyncWrite for TeeWriter<L, R> {
+    fn poll_write(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize, std::io::Error>> {
+        // Make a list of poll write futures
+        let this = self.get_mut();
+        let w_futures = [
+            Pin::new(&mut this.left).poll_write(cx, buf),
+            Pin::new(&mut this.right).poll_write(cx, buf),
+        ];
+
+        // iterate over each future
+        let mut num_bytes_written = 0;
+        for result in w_futures.into_iter() {
+            match result {
+                Poll::Ready(Ok(n)) => num_bytes_written = n, // continue to next writer future if current future is ready
+                Poll::Ready(Err(e)) => return Poll::Ready(Err(e)), // return error if current future has error
+                Poll::Pending => return Poll::Pending, // return pending if current future is pending
+            }
+        }
+
+        // return the number of bytes written
+        Poll::Ready(Ok(num_bytes_written))
     }
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.left.flush()?;
-        self.right.flush()?;
-        Ok(())
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), std::io::Error>> {
+        // Make a list of poll flush futures
+        let this = self.get_mut();
+        let w_futures = [
+            Pin::new(&mut this.left).poll_flush(cx),
+            Pin::new(&mut this.right).poll_flush(cx),
+        ];
+
+        // iterate over each future and return error if any future has error
+        for result in w_futures.into_iter() {
+            if let Poll::Ready(Err(e)) = result {
+                return Poll::Ready(Err(e));
+            }
+        }
+
+        // return ready if all futures are ready
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_shutdown(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), std::io::Error>> {
+        // Make a list of poll shutdown futures
+        let this = self.get_mut();
+        let w_futures = [
+            Pin::new(&mut this.left).poll_shutdown(cx),
+            Pin::new(&mut this.right).poll_shutdown(cx),
+        ];
+
+        // iterate over each future and return error if any future has error
+        for result in w_futures.into_iter() {
+            if let Poll::Ready(Err(e)) = result {
+                return Poll::Ready(Err(e));
+            }
+        }
+
+        // return ready if all futures are ready
+        Poll::Ready(Ok(()))
     }
 }
 
-fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+async fn handle_connection(stream: &mut TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     // Split stream to a buffered reader and a writer
-    let mut r_stream = BufReader::new(stream.try_clone()?);
-    let w_stream = stream;
+    let (r_stream, w_stream) = stream.split();
+    let mut r_stream = BufReader::new(r_stream);
 
     // Read the header
-    let http_request = String::from_utf8_lossy(&read_headers_buff(&mut r_stream)?).to_string();
+    let header_buff = read_headers_buff(&mut r_stream).await?;
+    let http_request = String::from_utf8_lossy(&header_buff);
     let http_request: Vec<&str> = http_request
         .lines()
         .take_while(|line| !line.is_empty())
@@ -122,7 +193,7 @@ fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error>> {
 
         // Read the body
         let mut buff = vec![0u8; content_length];
-        r_stream.read_exact(&mut buff)?;
+        r_stream.read_exact(&mut buff).await?;
         let body = String::from_utf8_lossy(&buff);
         println!("{}", body);
     }
@@ -135,50 +206,70 @@ fn handle_connection(stream: TcpStream) -> Result<(), Box<dyn Error>> {
         false => http_request.path,
     };
     let path = Path::new(RES_ROOT_FOLDER).join(format!(".{}", &path));
-    let (status, res_body, res_body_len) = match path.exists() {
-        true => {
-            let status = "200 OK";
-            let res_body = File::open(path)?;
-            let res_body_len = res_body.metadata()?.len() as usize;
-            (status, Box::new(res_body) as Box<dyn Read>, res_body_len)
-        }
-        false => {
-            let status = "404 Not Found";
-            let msg = b"NOT FOUND";
-            let res_body = Cursor::new(msg);
-            let res_body_len = msg.len();
-            (status, Box::new(res_body) as Box<dyn Read>, res_body_len)
-        }
+    let file = match path.exists() {
+        true => Some(File::open(path).await?),
+        false => None,
     };
-    let mut res_header = String::with_capacity(HEADER_BUFF_INIT_SIZE);
-    res_header.push_str(&format!("HTTP/1.1 {}\r\n", status).to_string());
-    res_header.push_str(&format!("Content-Length: {}\r\n", res_body_len));
-    res_header.push_str("\r\n");
-    let mut res = Cursor::new(res_header).chain(res_body);
+
+    // Write the response
+    const NOT_FOUND_STATUS: &str = "404 Not Found";
+    const NOT_FOUND_MSG: &str = "NOT FOUND";
+    const OK_STATUS: &str = "200 OK";
+    let mut res = String::with_capacity(HEADER_BUFF_INIT_SIZE);
+    res.push_str(&format!(
+        // Write the status line
+        "HTTP/1.1 {}\r\n",
+        match &file {
+            Some(_) => OK_STATUS,
+            None => NOT_FOUND_STATUS,
+        }
+    ));
+    res.push_str(&format!(
+        // Write the content length
+        "Content-Length: {}\r\n",
+        match &file {
+            Some(f) => f.metadata().await?.len() as usize,
+            None => NOT_FOUND_STATUS.len(),
+        }
+    ));
+    res.push_str("\r\n"); // End of header
+
+    // convert header to stream and chain with body of either a file or a string
+    let mut res = AsyncReadExt::chain(
+        Cursor::new(res),
+        match file {
+            Some(f) => Box::new(f) as Box<dyn AsyncRead + Unpin + Send>,
+            None => Box::new(Cursor::new(NOT_FOUND_MSG)) as Box<dyn AsyncRead + Unpin + Send>,
+        },
+    );
 
     // Write to both stream and console
     let mut tee_writer = TeeWriter::new(w_stream, stdout());
-    io::copy(&mut res, &mut tee_writer)?;
+    io::copy(&mut res, &mut tee_writer).await?;
     println!("");
 
     Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("0.0.0.0:80")?;
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("0.0.0.0:80").await?;
     println!("socket binded");
-    for stream in listener.incoming() {
-        println!("connection accepted");
-        let stream = match stream {
+    loop {
+        let (mut stream, addr) = match listener.accept().await {
             Err(e) => {
-                eprintln!("Error: {}", e);
+                eprintln!("Client connection error: {}", e);
                 continue;
             }
             Ok(s) => s,
         };
-        if let Err(e) = handle_connection(stream) {
-            eprintln!("Error: {}", e);
-        }
+        println!("connection from: {}", &addr);
+        task::spawn(async move {
+            if let Err(e) = handle_connection(&mut stream).await {
+                eprintln!("Error: {}, {}", &addr, e);
+            }
+            stream.shutdown().await.unwrap();
+            println!("connection closed for {}", &addr);
+        });
     }
-    Ok(())
 }
